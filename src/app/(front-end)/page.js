d92b0070d5a9c4d010f8448df3a07517cd880288
Towhid <Towhid@TOWHID>
2024-07-06 10:38:09 +0600

    f

---
+++ src/app/(front-end)/page.js
@@ -0,0 +1,14 @@
+import Banner from '@/components/front-end/Banner'
+import Navbar from '@/components/front-end/Navbar'
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <>
+        <Navbar/>
+       <div>
+       <Banner/>
+       </div>
+        </>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        "./node_modules/flowbite-react/lib/**/*.js",
+        "./public/**/*.html",
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,5 +16,7 @@ module.exports = {
             },
         },
     },
-    plugins: [require('daisyui')],
+    plugins: [
+        require("flowbite/plugin")
+    ],
 }
